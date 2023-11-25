ce89394db0380707322b01129b844bfa96250be7
mikhailvasilev <mall.87@mail.ru>
2023-11-25 15:07:14 +0300

    change app

--- HexletJavalin/src/main/java/org/example/hexlet/HelloWorld.java
+++ HexletJavalin/src/main/java/org/example/hexlet/HelloWorld.java
@@ -4,10 +4,9 @@ import io.javalin.Javalin;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        var app = Javalin.create(config -> {
-            config.plugins.enableDevLogging();
-        });
-        app.get("/", ctx -> ctx.result("Hello World!!!"));
+        var app = Javalin.create(config -> config.plugins.enableDevLogging());
+        app.get("/users", ctx -> ctx.result("GET /users"));
+        app.post("/users", ctx -> ctx.result("POST /users"));
         app.start(7070);
     }
 }
